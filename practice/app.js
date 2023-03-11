b43de18ceb4a5722bda3bd4c6f5cebb52efc730d
aldinhalilovic <aldinhalilovic06@gmail.com>
2023-03-11 12:46:55 +0100

    hoisting, this, algoritmi sa nizovima

--- practice/app.js
+++ practice/app.js
@@ -11,14 +11,139 @@
 // var y;
 // y = 10;
 
-console.log(squareFn(3));
+// console.log(squareFn(3));
 
-function squareFn(n) {
-  return n * n;
-}
+// function squareFn(n) {
+//   return n * n;
+// }
 
-console.log(squareConst(4));
+// console.log(squareConst(4));
 
-var squareConst = (n) => {
-  return n * n;
+// var squareConst = (n) => {
+//   return n * n;
+// };
+
+// var a;
+
+// let a;
+
+// console.log(a);
+
+// a = 10;
+
+// console.log(a);
+
+// console.log(square(3));
+
+// function square(n) {
+//   return n * n;
+// }
+// console.log(constSquare(3));
+
+// const constSquare = function (n) {
+//   return n * n;
+// };
+
+// const first = {
+//   // name: "key",
+//   key: "value",
+//   phone: "xaomi",
+//   friends: ["joey", "chandler", "ross"],
+//   town: "Novi Pazar",
+//   anotherObject: {
+//     tv: "samsung",
+//     laptop: "acer",
+//     thisObject() {
+//       console.log(this, "manji objekat");
+//     },
+//   },
+//   thisObject() {
+//     console.log(this.anotherObject.tv, "clg iz objekta");
+//   },
+// };
+
+// console.log(first);
+// first.thisObject();
+// first.anotherObject.thisObject();
+
+// const second = {
+//   key: "value",
+//   name: "aldin",
+//   age: 21,
+//   knowledge: {
+//     javascript: "good",
+//     html: "good",
+//     css() {
+//       console.log(this.advanced.josJedan);
+//     },
+//     advanced: {
+//       react: "good",
+//       typescript: "solid",
+//       reactnative: "beginner",
+//       josJedan: {
+//         paJosJedan: {
+//           onda: {
+//             poslednji: "nesto",
+//           },
+//           sayHello() {
+//             console.log(this);
+//           },
+//         },
+//       },
+//     },
+//   },
+// };
+
+// console.log(second.knowledge.advanced.josJedan.paJosJedan.onda);
+// second.knowledge.css();
+
+// function callFunkcija(arg1, arg2) {
+//   console.log(this.age + arg1 + arg2);
+// }
+
+// callFunkcija.call(second, "godina", " call");
+// callFunkcija.apply(second, ["godina", "    apply"]);
+// const bindFunkcija = callFunkcija.bind(second, "godina", "     bind");
+
+// bindFunkcija();
+
+// \
+// Prebrojati koliko se puta u unetom stringu pojavljuje slovo "M" (i veliko i malo). Npr. za
+// "Mama ima momu", dobija se rezultat 5.
+
+let string = "Mama ima momu";
+
+const checkLetter = (someString) => {
+  let numberMs = 0;
+  for (let m in someString) {
+    // console.log(someString[m]);
+    if (someString[m] === "m" || someString[m] === "M") {
+      numberMs += 1;
+    }
+  }
+  return numberMs;
 };
+
+// console.log(checkLetter(string));
+
+// Ispitati da li u unetom stringu ima više malih ili velikih slova. Slova koja se koriste u stringu
+// pripadaju isključivo engleskoj abecedi.
+
+let stringNeki = "Ako JA SAD ovako PISEM koliko CE MALIH slova DA ima";
+
+const checkCases = (string) => {
+  let malaSlova = 0;
+  let velikaSlova = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== " ") {
+      if (string[i] === string[i].toUpperCase()) {
+        velikaSlova++;
+      } else malaSlova++;
+    }
+  }
+
+  return `Malih slova ima ${malaSlova}, a velikih ${velikaSlova}`;
+};
+
+console.log(checkCases(stringNeki));
