2236b77a252facb9a47c5c224428c739c9db5638
aldinhalilovic <aldinhalilovic06@gmail.com>
2023-03-23 12:30:43 +0100

    promisi, promis all, fetchanje podataka

--- asynchronous/app.js
+++ asynchronous/app.js
@@ -43,68 +43,144 @@
 
 // nekaFunkcija();
 
-function time() {
-  setTimeout(() => {
-    console.log("nesto");
-  }, 1000);
+// function one() {
+//   console.log(1);
+// }
+
+// function two() {
+//   setTimeout(() => {
+//     console.log("aslkdskldisdisudio");
+//   }, -2);
+// }
+
+// function three() {
+//   setTimeout(() => {
+//     console.log(3);
+//   }, 1);
+// }
+
+// function neka() {
+//   console.log("imas,dnaskldj");
+// }
+
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// two();
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+
+//////////////////////////////////////////   PROMISI   /////////////////////////////////////////////////////////////////////
+
+// function time() {
+//   setTimeout(() => {
+//     console.log("nesto");
+//   }, 1000);
+// }
+
+// function one() {
+//   console.log(1);
+// }
+
+// function two() {
+//   console.log(2);
+// }
+
+// one();
+// time();
+// two();
+
+// const obecanje = new Promise((resolve, reject) => {
+//   // resolve(2);
+//   // reject("neuspesno");
+//   let a = 1 + 2;
+//   if (a === 3) {
+//     resolve(a);
+//   } else {
+//     reject("Zbir je netacan");
+//   }
+// });
+
+// obecanje
+//   .then((res) => {
+//     let a = 0;
+//     for (let i = 0; i < res; i++) {
+//       a += 2;
+//     }
+//     // res + 10;
+//     return a;
+//   })
+//   .then((el) => console.log(el))
+//   .catch((el) => console.log(el))
+//   .finally(() => console.log("konacan rezultat"));
+
+// const prvi = new Promise((resolve, reject) => {
+//   // resolve("prosao prvi");
+//   reject("ne prolazi");
+// });
+// const drugi = new Promise((resolve, reject) => {
+//   resolve("prosao drugi");
+// });
+// const treci = new Promise((resolve, reject) => {
+//   resolve("prosao treci");
+// });
+
+// Promise.all([prvi, drugi, treci])
+//   .then((el) => console.log(el))
+//   .catch((el) => console.log(el));
+
+// fetch("httpsppppppppppp://dummyjson.com/products")
+//   .then((res) => res.json())
+//   .then((el) => (a = el))
+//   .then(() => console.log(a))
+//   .catch((error) => console.log("Greska " + error));
+
+function getData() {
+  let a;
+  fetch("https://dummyjson.com/products")
+    .then((res) => res.json())
+    .then((el) => (a = el))
+    .then(() => console.log(a.products[0].images))
+    .catch((error) => console.log("Greska " + error));
+
+  return a;
 }
 
-function one() {
-  console.log(1);
-}
-
-function two() {
-  setTimeout(() => {
-    console.log("aslkdskldisdisudio");
-  }, -2);
-}
-
-function three() {
-  setTimeout(() => {
-    console.log(3);
-  }, 1);
-}
-
-function neka() {
-  console.log("imas,dnaskldj");
-}
-
-console.log(2);
-console.log(2);
-console.log(2);
-two();
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
+const data = getData();
+// console.log(data);
+// if (data) {
+//   console.log(data, "aa");
+// }
